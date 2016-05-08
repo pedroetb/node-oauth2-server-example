148f3ba44f9f327cd1670f4379c8120010e77a87
Pedro Trujillo <pedroetb@gmail.com>
2016-05-08 20:07:49 +0100

    Initialize repository

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    oauthserver = require('oauth2-server');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.oauth = oauthserver({
+  model: {}, // See below for specification
+  grants: ['password'],
+  debug: true
+});
+
+app.all('/oauth/token', app.oauth.grant());
+
+app.get('/', app.oauth.authorise(), function (req, res) {
+  res.send('Secret area');
+});
+ 
+app.use(app.oauth.errorHandler());
+
+app.listen(3000);
